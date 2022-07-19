8c24e28ce721c90ef3f0ededfe4e47539de966c2
idnm <nnemkov@gmail.com>
2022-07-19 11:45:08 +0300

    Plots, num_gd_iterations, complexity target.

--- traps.py
+++ traps.py
@@ -29,11 +29,17 @@ class ExperimentOptions:
     num_targets: int
     num_samples: int
     fixed_unitary: jnp.ndarray = None
+    fixed_complexity: int = None
     keep_history: bool = False
+    num_gd_iterations: int = 2000
 
     def __post_init__(self):
-        assert self.target_type in ['self_instance', 'random_unitary', 'fixed_unitary']
-        assert self.ansatz_type in ['cz', 'cx', 'cp']
+        assert self.target_type in ['self_instance', 'random_unitary', 'fixed_unitary', 'cz_fixed_complexity']
+        assert self.ansatz_type in ['cz', 'cp']
+        if self.target_type == 'fixed_unitary':
+            assert self.fixed_unitary is not None
+        if self.target_type == 'cz_fixed_complexity':
+            assert self.fixed_complexity is not None
 
 
 class Experiment:
@@ -61,13 +67,23 @@ class Experiment:
             u_target = unitary_group.rvs(2 ** self.options.num_qubits, random_state=random_seed)
         elif self.options.target_type == 'fixed_unitary':
             u_target = self.options.fixed_unitary
+        elif self.options.target_type == 'cz_fixed_complexity':
+            num_gates_target = self.options.fixed_complexity
+            anz_target = Ansatz(self.options.num_qubits, self.options.ansatz_type, fill_layers(connected_layer(self.options.num_qubits), num_gates_target))
+            angles_target = random_angles(subkey, anz_target.num_angles)
+            u_target = anz_target.unitary(angles_target)
 
         key, subkey = random.split(key)
         initial_angles = random_angles(subkey, self.options.num_samples * anz.num_angles).reshape((self.options.num_samples, anz.num_angles))
         initial_angles = [A(angles) for angles in initial_angles]
         loss = lambda a: cost_HST(u_target, anz.unitary(a.angles))
 
-        result = mynimize(loss, initial_angles, OptOptions(num_iterations=2000, learning_rate=0.1, keep_history=self.options.keep_history))
+        opt_options = OptOptions(num_iterations=self.options.num_gd_iterations, learning_rate=0.1, keep_history=self.options.keep_history)
+        result = mynimize(
+            loss,
+            initial_angles,
+            opt_options)
+
         if not self.options.keep_history:
             del result.all_results
         self.results[num_gates, num_target] = result
@@ -93,7 +109,7 @@ class Experiment:
         success_ratios = {key: self.success_ratio_from_losses(res.all_best_losses, threshold_loss, min_loss) for key, res in self.results.items()}
         return success_ratios
 
-    def plot_success_ratios(self, threshold_loss=1e-4, min_loss=None):
+    def get_mean_and_std_success_ratios(self, threshold_loss=1e-4, min_loss=None):
         success_ratios = self.get_success_ratios(threshold_loss, min_loss)
         mean_success_ratios = []
         std_success_ratios = []
@@ -101,9 +117,22 @@ class Experiment:
             success_ratios_at_num_gates = jnp.array([success_ratios[num_gates, num_target] for num_target in range(self.options.num_targets)])
             mean_success_ratios.append(jnp.mean(success_ratios_at_num_gates))
             std_success_ratios.append(jnp.std(success_ratios_at_num_gates))
+        return jnp.array(mean_success_ratios), jnp.array(std_success_ratios)
 
-        plt.errorbar(self.options.gate_counts, mean_success_ratios, yerr=std_success_ratios, fmt='o', capsize=10);
-        plt.title(f'{self.options.target_type} {self.options.ansatz_type} {self.options.num_qubits}q')
+    def plot_success_ratios(self, threshold_loss=1e-4, min_loss=None, maketitle=True, label='', xshift=0):
+
+        mean_success_ratios, std_success_ratios = self.get_mean_and_std_success_ratios(threshold_loss, min_loss)
+        gate_counts = jnp.array(self.options.gate_counts)+xshift
+        plt.errorbar(
+            gate_counts,
+            mean_success_ratios,
+            yerr=std_success_ratios,
+            fmt='o',
+            capsize=10,
+            label=label);
+
+        if maketitle:
+            plt.title(f'{self.options.target_type} {self.options.ansatz_type} {self.options.num_qubits}q')
         plt.yscale('log')
         plt.xticks(self.options.gate_counts, rotation=70)
         plt.ylabel('Success Ratio')
